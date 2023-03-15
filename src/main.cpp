0d5172d5331e91b032d7992fc6f1a96ef9a77813
Chanadu <chandu.peddada@gmail.com>
2023-03-14 19:53:28 -0500

    Full Connect 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include "iostream"
+#include "string"
 
 const int SCREEN_WIDTH = 1600;
 const int SCREEN_HEIGHT = 1000;
@@ -11,24 +12,29 @@ const int BOARD_COLUMN_SIZE = SCREEN_HEIGHT / BOARD_SIZE_Y;
 const Color PLAYER_1_COLOR = Color{182, 191, 11, 255};
 const Color PLAYER_2_COLOR = Color{196, 8, 24, 255};
 
-enum BoardTileState
+enum BoardTileStates
 {
     EMPTY,
     PLAYER_1,
     PLAYER_2
 };
-BoardTileState board[BOARD_SIZE_X][BOARD_SIZE_Y];
+BoardTileStates board[BOARD_SIZE_X][BOARD_SIZE_Y];
 
-enum GameState
+enum GameStates
 {
-    PLAYER_1_TURN,
-    PLAYER_2_TURN,
-    GAME_OVER
+    PLAYER_1_TURN = 1,
+    PLAYER_2_TURN = 2,
+    GAME_OVER_PLAYER_1 = -1,
+    GAME_OVER_PLAYER_2 = -2,
+    GAME_OVER_DRAW = 0
 };
-GameState gameState;
+GameStates gameState = PLAYER_1_TURN;
+
+int totalClicks = 0;
 
 static void numberClicked(int num);
 static void setUpBoard();
+static GameStates isGameOver();
 
 int main(void)
 {
@@ -56,11 +62,13 @@ int main(void)
         if (IsKeyPressed(KEY_SEVEN))
             numberClicked(7);
 
+        gameState = isGameOver();
+
         //----------------------------------------------------------------------------------
 
         BeginDrawing();
 
-        ClearBackground(Color{14, 143, 230});
+        ClearBackground(Color{181, 181, 181, 255});
 
         for (int i = 0; i < BOARD_SIZE_X; i++)
         {
@@ -69,31 +77,42 @@ int main(void)
                 Rectangle rec = {(float)(i * BOARD_ROW_SIZE), (float)(j * BOARD_COLUMN_SIZE), BOARD_ROW_SIZE, BOARD_COLUMN_SIZE};
                 DrawRectangleLinesEx(rec, 5, Color{0, 0, 0, 255});
                 Color color;
-                if (board[i][j] != BoardTileState::EMPTY)
+                if (board[i][j] != BoardTileStates::EMPTY)
                 {
-                    color = board[i][j] == BoardTileState::PLAYER_1 ? PLAYER_1_COLOR : PLAYER_2_COLOR;
+                    color = board[i][j] == BoardTileStates::PLAYER_1 ? PLAYER_1_COLOR : PLAYER_2_COLOR;
                     DrawCircle(i * BOARD_ROW_SIZE + BOARD_ROW_SIZE / 2, j * BOARD_COLUMN_SIZE + BOARD_COLUMN_SIZE / 2, BOARD_COLUMN_SIZE / 2 - 20, color);
                 }
             }
         }
 
+        if (gameState <= 0)
+        {
+            const char *str = gameState == GameStates::GAME_OVER_PLAYER_1   ? "PLAYER 1 (YELLOW) WINS"
+                              : gameState == GameStates::GAME_OVER_PLAYER_2 ? "PLAYER 2 (RED) WINS"
+                                                                            : "GAME OVER";
+            DrawText(str, 150, SCREEN_HEIGHT / 3, 110, Color{255, 0, 0, 255});
+        }
+
+        DrawFPS(20, 20);
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
 
 static void numberClicked(int num)
 {
-    if (gameState == GameState::GAME_OVER)
+    if (gameState <= 0)
         return;
     num--;
     for (int i = BOARD_SIZE_Y - 1; i >= 0; i--)
     {
-        if (board[num][i] == BoardTileState::EMPTY)
+        if (board[num][i] == BoardTileStates::EMPTY)
         {
-            board[num][i] = gameState == GameState::PLAYER_1_TURN ? BoardTileState::PLAYER_1 : BoardTileState::PLAYER_2;
-            gameState = gameState == GameState::PLAYER_1_TURN ? GameState::PLAYER_2_TURN : GameState::PLAYER_1_TURN;
+            board[num][i] = gameState == GameStates::PLAYER_1_TURN ? BoardTileStates::PLAYER_1 : BoardTileStates::PLAYER_2;
+            gameState = gameState == GameStates::PLAYER_1_TURN ? GameStates::PLAYER_2_TURN : GameStates::PLAYER_1_TURN;
+            totalClicks++;
             break;
         }
     }
@@ -105,7 +124,93 @@ static void setUpBoard()
     {
         for (int j = 0; j < BOARD_SIZE_Y; j++)
         {
-            board[i][j] = BoardTileState::EMPTY;
+            board[i][j] = BoardTileStates::EMPTY;
         }
     }
+}
+
+static GameStates isGameOver()
+{
+    if (gameState <= 0)
+        return gameState;
+
+    if (totalClicks == BOARD_SIZE_X * BOARD_SIZE_Y)
+    {
+        return GameStates::GAME_OVER_DRAW;
+    }
+
+    // horizontalCheck
+    for (int j = 0; j < BOARD_SIZE_Y - 3; j++)
+    {
+        for (int i = 0; i < BOARD_SIZE_X; i++)
+        {
+            if (board[i][j] == BoardTileStates::PLAYER_1 &&
+                board[i][j + 1] == BoardTileStates::PLAYER_1 &&
+                board[i][j + 2] == BoardTileStates::PLAYER_1 &&
+                board[i][j + 3] == BoardTileStates::PLAYER_1)
+                return GameStates::GAME_OVER_PLAYER_1;
+
+            if (board[i][j] == BoardTileStates::PLAYER_2 &&
+                board[i][j + 1] == BoardTileStates::PLAYER_2 &&
+                board[i][j + 2] == BoardTileStates::PLAYER_2 &&
+                board[i][j + 3] == BoardTileStates::PLAYER_2)
+                return GameStates::GAME_OVER_PLAYER_2;
+        }
+    }
+    // verticalCheck
+    for (int i = 0; i < BOARD_SIZE_X - 3; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE_Y; j++)
+        {
+            if (board[i][j] == BoardTileStates::PLAYER_1 &&
+                board[i + 1][j] == BoardTileStates::PLAYER_1 &&
+                board[i + 2][j] == BoardTileStates::PLAYER_1 &&
+                board[i + 3][j] == BoardTileStates::PLAYER_1)
+                return GameStates::GAME_OVER_PLAYER_1;
+
+            if (board[i][j] == BoardTileStates::PLAYER_2 &&
+                board[i + 1][j] == BoardTileStates::PLAYER_2 &&
+                board[i + 2][j] == BoardTileStates::PLAYER_2 &&
+                board[i + 3][j] == BoardTileStates::PLAYER_2)
+                return GameStates::GAME_OVER_PLAYER_2;
+        }
+    }
+    // ascendingDiagonalCheck
+    for (int i = 3; i < BOARD_SIZE_X; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE_Y - 3; j++)
+        {
+            if (board[i][j] == BoardTileStates::PLAYER_1 &&
+                board[i - 1][j + 1] == BoardTileStates::PLAYER_1 &&
+                board[i - 2][j + 2] == BoardTileStates::PLAYER_1 &&
+                board[i - 3][j + 3] == BoardTileStates::PLAYER_1)
+                return GameStates::GAME_OVER_PLAYER_1;
+
+            if (board[i][j] == BoardTileStates::PLAYER_2 &&
+                board[i - 1][j + 1] == BoardTileStates::PLAYER_2 &&
+                board[i - 2][j + 2] == BoardTileStates::PLAYER_2 &&
+                board[i - 3][j + 3] == BoardTileStates::PLAYER_2)
+                return GameStates::GAME_OVER_PLAYER_2;
+        }
+    }
+    // descendingDiagonalCheck
+    for (int i = 3; i < BOARD_SIZE_X; i++)
+    {
+        for (int j = 3; j < BOARD_SIZE_Y; j++)
+        {
+            if (board[i][j] == BoardTileStates::PLAYER_1 &&
+                board[i - 1][j - 1] == BoardTileStates::PLAYER_1 &&
+                board[i - 2][j - 2] == BoardTileStates::PLAYER_1 &&
+                board[i - 3][j - 3] == BoardTileStates::PLAYER_1)
+                return GameStates::GAME_OVER_PLAYER_1;
+
+            if (board[i][j] == BoardTileStates::PLAYER_2 &&
+                board[i - 1][j - 1] == BoardTileStates::PLAYER_2 &&
+                board[i - 2][j - 2] == BoardTileStates::PLAYER_2 &&
+                board[i - 3][j - 3] == BoardTileStates::PLAYER_2)
+                return GameStates::GAME_OVER_PLAYER_2;
+        }
+    }
+
+    return gameState;
 }
